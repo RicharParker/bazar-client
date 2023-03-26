0757e8f6cb906a9b96d860b848806756663e2a09
Angel <parkerrichar307@gmail.com>
2023-03-26 17:51:38 -0600

    Paginas agregadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import Home from "./pages/Home";
 import {
   BrowserRouter as Router,
   Route,
 } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Success from "./pages/Success";
 
 const App = () => {
 
@@ -10,6 +15,21 @@ const App = () => {
     <Router>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="/products">
+          <ProductList />
+        </Route>
+        <Route path="/product">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+        <Route path="/login">
+          <Login />
         </Route>
     </Router>
   );
